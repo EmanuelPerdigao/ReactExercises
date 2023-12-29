2108eca736b57e58eb5afa479eb061eca9a11706
emanuel perdigao casa <emanuelfsperdigao@gmail.com>
2023-12-29 16:10:24 +0000

    useState exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Employee from './components/Employee';
+import {useState} from 'react';
 
 function App() {
-  console.log("TEST");
+  
+  const[employeeRole, setEmployeeRole] = useState('none');
   const showEmployees = true;
+
   return (
     <div className="App">
       {showEmployees ? 
 
-        <Employee></Employee>
+        <>
+        <input type='text' onChange={(e) => {
+
+          console.log(e.target.value);
+          setEmployeeRole(e.target.value);
+
+
+        }}/>
+        <Employee name = "Caleb" role = {employeeRole}></Employee>
+        </>
 
       :
         <p>You cannot see the employees</p>
